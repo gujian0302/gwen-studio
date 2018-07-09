ac58ff1d5de5e5c10f30572ae04b183d7ddc7a34
gujian <gujian32@gmail.com>
2018-07-09 18:50:32 +0800

    sketch

--- gwen-admin/web/.roadhogrc.mock.js
+++ gwen-admin/web/.roadhogrc.mock.js
@@ -1,3 +1,4 @@
 
 export default {
+  'GET /style/list-all': [{id:1, name: '新中式', ordinate: 1}]
 };
--- gwen-admin/web/src/utils/host.js
+++ gwen-admin/web/src/utils/host.js
@@ -1,6 +1,9 @@
 
 if (process.env.NODE_ENV === 'production') {
     module.exports = `https://www.baidu.com`
-}else {
-    module.exports = `http://192.168.0.109:8080`
-}
+}else{
+  module.exports = 'http://localhost:8000'
+ } 
+// }else {
+//     module.exports = `http://192.168.0.109:8080`
+// }
